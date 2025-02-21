a834d7423393dca0510e17045ae541b3e64f6d00
Asta Rem <asta.rem@hotmail.com>
2025-02-21 20:38:11 +0200

    added colors, started adding input html

---
+++ classes/Category.js
@@ -0,0 +1,10 @@
+// Category Klasė
+// Aprašymas: Klasė, kuri reprezentuoja knygų kategoriją.
+// Savybės:
+// id: unikalus kategorijos numeris
+// categoryName: kategorijos pavadinimas.
+// books: masyvas, kuriame saugomos knygos.
+// Metodai:
+// Geteriai, seteriai
+// getBooksByAuthor(authorName): grąžina masyvą knygų, kurių autorius atitinka duotą vardą.
+// getBooksByPriceRange(minPrice, maxPrice): grąžina knygas, kurių kaina yra nustatytame intervale.
---
+++ classes/Reader.js
@@ -0,0 +1,14 @@
+// Reader (Skaitytojas) Klasė
+// Aprašymas: Klasė, kuri reprezentuoja bibliotekos skaitytoją.
+// Savybės:
+// name: skaitytojo vardas.
+// reader_id: unikalus skaitytojo identifikatorius.
+// borrowed_books: masyvas, kuriame saugomos pasiskolintos knygos.
+// Metodai:
+// getName(): grąžina skaitytojo vardą.
+// setName(name): nustato skaitytojo vardą.
+// getReaderId(): grąžina skaitytojo ID.
+// setReaderId(reader_id): nustato skaitytojo ID.
+// borrowBook(book): prideda knygą į pasiskolintų knygų sąrašą, jei ji yra prieinama.
+// returnBook(book): pašalina knygą iš pasiskolintų knygų sąrašo.
+// getBorrowedBooks(): grąžina visų pasiskolintų knygų masyvą.
