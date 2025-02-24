6421b4ae1a499f497227292f6391570d99dfe2c7
Asta Rem <asta.rem@hotmail.com>
2025-02-24 20:58:48 +0200

    added Library class

--- classes/Library.js
+++ classes/Library.js
@@ -4,3 +4,40 @@
 // categories: masyvas, kuriame saugomos kategorijos objektai.
 // Metodai:
 // Geteriai, seteriai
+
+
+// nauja kategorija kuria Library klase
+class Library {
+    #categories = [];
+    constructor (library){
+        this.library = library;
+    }
+
+    setCategory(category){
+        let newCategory = new Category(category);
+        this.#categories.push(newCategory);
+    }
+
+    getCategories(categories){
+        return this.#categories;
+    }
+
+}
+
+
+
+
+class Category{
+    constructor(categoryName){
+        this.category = categoryName;
+    }
+}
+
+
+let pietinioBiblioteka = new Library("pietinio biblioteka")
+console.log(pietinioBiblioteka);
+pietinioBiblioteka.setCategory("poezija");
+pietinioBiblioteka.setCategory("grozine literatura");
+
+let kategorijos = pietinioBiblioteka.getCategories();
+console.log(kategorijos);
