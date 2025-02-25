a488f2d49ee5b15dcabf9f7f484d3250afb1b6a5
ArnasJankunas <arnasjankunas123@gmail.com>
2025-02-25 18:40:27 +0200

    Rating klaseje ideti statiniai kintamieji kurie renka knygos reitinga ir surenka komentarus. Book klaseje nedaryta nieko

--- classes/Rating.js
+++ classes/Rating.js
@@ -1,49 +1,43 @@
 class Rating {
-    constructor(id, bookId, readerId, rating, comment) {
-        this.id = id;
+    static COUNTER = 0; 
+    static COMMENTS = []; 
+    static LAST_ID = 0;
+
+    constructor(bookId, readerId, rating, comment) {
+        this.id = Rating.generateUniqueId(); 
         this.bookId = bookId;
         this.readerId = readerId;
-        this.rating = rating; 
-        this.comment = comment;  
+        this.setRating(rating);
+        this.comment = comment || "Komentaro nėra";  
 
+        Rating.COUNTER++; 
+        Rating.COMMENTS.push(this.comment);
     }
 
-    // Get'eriai
-
-    getid() {
-        return this.id;
+    // 📌 Get'eriai
+    getId(){ 
+        return this.id; 
     }
-    
-    getBookId() {
+    getBookId(){ 
         return this.bookId;
+     }
+    getReaderId(){ 
+        return this.readerId; 
     }
-    
-    getReaderId() {
-        return this.readerId;
-    }
-    
-    getRating() {
+    getRating(){ 
         return this.rating;
     }
-    
-    getComment() {
-        return this.comment;
+    getComment(){ 
+        return this.comment; 
     }
-    
-    // Set'eriai
-    
-    setId(id) {
-        this.id = id;
+
+    // 📌 Set'eriai
+    setBookId(bookId){ 
+        this.bookId = bookId; 
     }
-    
-    setBookId(bookId) {
-        this.bookId = bookId;
-    }
-    
-    setReaderId(readerId) {
+    setReaderId(readerId) { 
         this.readerId = readerId;
-    }
-    
+     }
     setRating(rating) {
         if (rating >= 1 && rating <= 5) {
             this.rating = rating;
@@ -51,9 +45,32 @@ class Rating {
             console.log("Įvertinimas turi būti tarp 1 ir 5.");
         }
     }
-    
-    setComment(comment) {
-        this.comment = comment;
+    setComment(comment) { 
+        this.comment = comment || "Komentaro nėra"; 
+    }
+
+    static generateUniqueId() {
+        return ++Rating.LAST_ID;
+    }
+
+    static getCounter() {
+        return Rating.COUNTER;
+    }
+
+
+    static getAllComments() {
+        return Rating.COMMENTS;
     }
 }
 
+// --- Testavimas ---
+//const review1 = new Rating(202, 5, 4, "Labai gera knyga!");
+//const review2 = new Rating(203, 8, 3, "Vidutiniška.");
+//const review3 = new Rating(204, 10, 5); // Be komentaro
+
+//console.log("Atsiliepimas 1 ID:", review1.getId());
+//console.log("Atsiliepimas 2 ID:", review2.getId());
+//console.log("Atsiliepimas 3 ID:", review3.getId());
+
+//console.log("Viso sukurta atsiliepimų:", Rating.getCounter());
+c//onsole.log("Visi komentarai:", Rating.getAllComments());
