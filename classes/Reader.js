268374cf3b7a80e8a7b9c68d3ec353d4a4943d5f
Asta Rem <asta.rem@hotmail.com>
2025-02-25 17:24:11 +0200

    Roberto kodas Reader + Category

--- classes/Reader.js
+++ classes/Reader.js
@@ -12,3 +12,52 @@
 // borrowBook(book): prideda knygą į pasiskolintų knygų sąrašą, jei ji yra prieinama.
 // returnBook(book): pašalina knygą iš pasiskolintų knygų sąrašo.
 // getBorrowedBooks(): grąžina visų pasiskolintų knygų masyvą.
+
+
+class Reader {
+ 
+    #name;
+    #readerId;
+    #borrowed_books;
+ 
+    constructor(name, readerId) {
+        this.#name = name;
+        this.#readerId = readerId;
+        this.#borrowed_books = [];
+    }
+ 
+    getName() {
+        return this.#name;
+    }
+ 
+    getReaderId() {
+        return this.#readerId;
+    }
+ 
+    getBorrowedBooks() {
+        return this.#borrowed_books;
+    }
+ 
+    setName(newName) {
+        this.#name = newName;
+    }
+ 
+    setReaderId(newReaderId) {
+        this.#readerId = newReaderId;
+    }
+ 
+    borrowBook(book) {
+        if (book && !this.#borrowed_books.includes(book)) { // Tikrinam, ar knyga yra bibliotekoj ir ar nėra pasiskolinta
+            this.#borrowed_books.push(book);
+            console.log(`${this.#name} pasiskolino knygą: ${book.getTitle()}`);
+        } else {
+            console.log('Knyga yra jau pasiskolinta .');
+        }
+    }
+ 
+ 
+    returnBook(book) {
+        this.#borrowed_books = this.#borrowed_books.filter(a => a !== book); // Paliekame tik tas knygas, kurios nėra grąžinama
+        console.log(`${this.#name} grąžino knygą: ${book.getTitle()}`);
+    }
+}
